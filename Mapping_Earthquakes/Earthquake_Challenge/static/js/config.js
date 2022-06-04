0bc12fc90c30de966408982aa3c7e0eeb570d67e
allisonorourke-ufGfGy <100240594+allisonorourke-ufGfGy@users.noreply.github.com>
2022-06-03 22:20:23 -0400

    Delete Mapping_Earthquakes directory

--- Mapping_Earthquakes/Earthquake_Challenge/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoiYWx5b3JvdXJrZTEiLCJhIjoiY2wzOWl1Z2o2MGEzYzNpbXJpd2I5OHZ0YyJ9.ksBbQ5fKdg2MwvptBKGfbw";
--- Mapping_Earthquakes/Simple_Map/static/js/config.js
+++
@@ -1,1 +0,0 @@
-const API_KEY = "pk.eyJ1IjoiYWx5b3JvdXJrZTEiLCJhIjoiY2wzOWl1Z2o2MGEzYzNpbXJpd2I5OHZ0YyJ9.ksBbQ5fKdg2MwvptBKGfbw";
